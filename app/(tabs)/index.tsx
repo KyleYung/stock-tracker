8b8c65f9127b0d4091d6d9704a54ca0508c46692
KyleYung <kylehyung@gmail.com>
2026-03-09 12:39:01 -0400

    test homescreen

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,16 @@
+import { Text, View } from "react-native";
+
+export default function HomeScreen() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "white",
+      }}
+    >
+      <Text style={{ fontSize: 24 }}>Stock Tracker</Text>
+    </View>
+  );
+}
